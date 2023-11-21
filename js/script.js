5639c1963c60c387e7ca727990cd3f45cf78e7d9
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-21 15:34:32 +0100

    add: exercise complete

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            arrayEmail:[],
+            numberOfEmail: "",
+            inputNumberOfEmail: ""
         }
     },
     methods: {
-        
-    }
+        generateRndMail() {
+            this.arrayEmail = [];
+            this.numberOfEmail = this.inputNumberOfEmail
+            for (let i = 0; i < this.numberOfEmail; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    this.arrayEmail.push(resp.data.response);
+                }) 
+            }
+        },
+    },
 }).mount("#app")

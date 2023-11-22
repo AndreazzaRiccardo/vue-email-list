b537e376728ab007639c24c59f50b2d3925e4177
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-22 10:24:44 +0100

    add: exercise correction

--- js/script.js
+++ js/script.js
@@ -5,19 +5,27 @@ createApp({
         return {
             arrayEmail:[],
             numberOfEmail: "",
-            inputNumberOfEmail: ""
+            inputNumberOfEmail: "",
+            isLoading: false
         }
     },
     methods: {
         generateRndMail() {
-            this.arrayEmail = [];
-            this.numberOfEmail = this.inputNumberOfEmail
-            for (let i = 0; i < this.numberOfEmail; i++) {
-                axios
-                .get("https://flynn.boolean.careers/exercises/api/random/mail")
-                .then((resp) => {
-                    this.arrayEmail.push(resp.data.response);
-                }) 
+            if(this.inputNumberOfEmail != 0) {
+                this.isLoading = true;
+                this.arrayEmail = [];
+                this.numberOfEmail = this.inputNumberOfEmail;
+                for (let i = 0; i < this.numberOfEmail; i++) {
+                    axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                        this.arrayEmail.push(resp.data.response);
+                        if(this.arrayEmail.length === this.numberOfEmail){
+                            this.isLoading = false;
+                        }
+                    }) 
+                }
+                this.inputNumberOfEmail = "";
             }
         },
     },
